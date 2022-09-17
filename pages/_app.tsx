8d24167d6c7cdefd90be94c808aca0059071f9a7
anuuyu <yusrandante@gmail.com>
2022-09-17 11:59:49 +0800

    :rocket:

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import { NextUIProvider } from '@nextui-org/react'
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+  <NextUIProvider>
+  <Component {...pageProps} />
+  </NextUIProvider>
+  ); 
+}
+
+export default MyApp
