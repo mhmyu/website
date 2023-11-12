71e3913640775f7aa1f9c42a192192be0ef0791d
mhmyu <mhmyu@pm.me>
2023-11-13 04:06:41 +0800

    correct imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
-import { GeistSans, GeistMono } from 'geist/font'
+import { GeistSans } from 'geist/font/sans'
+import { GeistMono } from 'geist/font/mono'
 
 export const metadata = {
   title: 'MHM',
